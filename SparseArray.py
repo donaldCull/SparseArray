56a4828a44a11a657ae57faaedfcacb37f8f0ae3
donald <donaldjames.cull@my.jcu.edu.au>
2018-04-01 13:22:58 +1000

    Sparse array getters/setters tested and working.  Matches assignment outputs

--- SparseArray.py
+++ SparseArray.py
@@ -8,25 +8,32 @@ class SparseArray:
             self._element = element
             self._next = next
 
-#------------------------------- sparse array methods -------------------------------
+        def __str__(self):
+            return str(self._element)  # Inside your node class
+
+        #------------------------------- sparse array methods -------------------------------
 
     def __init__(self, size):
         self._data = [None] * size
         self._head = None
         self._size = size
+        self._usage = 0
 
     def __len__(self):
         return self._size
 
     def __getitem__(self, item):
-        pass
+        return self._data[item]
 
     def __setitem__(self, key, value):
-
-        pass
+        """Add element e to the top of the stack."""
+        newest = self._Node(value, self._head)  # create and link a new node
+        self._data[key] = newest
+        self._head = newest
+        self._usage += 1
 
     def get_usage(self):
-        pass
+        return self._usage
 
     def fill(self, sequence):
         pass
--- use_sparse_array.py
+++ use_sparse_array.py
@@ -1,6 +1,13 @@
 from SparseArray import SparseArray
 
-S = SparseArray(10)
-print(len(S))
+array = SparseArray(1000000)
+print(len(array))
+print(array.get_usage())
+array[999999] = 42
+print(array[999999])
+print(array[-1])
+print(array.get_usage())
+print(array[0])
+print(array[0] is None)
+print(array[1000000])
 
-list
