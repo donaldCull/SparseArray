b2218839c4e18000422c27792a775a97d42b3eaf
donald <donaldjames.cull@my.jcu.edu.au>
2018-04-03 17:37:35 +1000

    fixed bug in fill method.  New items are added as Node objects.  Added experiments for Sparse array efficiency

--- SparseArray.py
+++ SparseArray.py
@@ -1,60 +1,110 @@
 class SparseArray:
+    """
+    Implementation of SparseArray utilising private Node subclass
+    """
 
     # -------------------------- nested Node class --------------------------
     class _Node:
+        """
+        Private Node class used to fill sparse array
+        """
         __slots__ = '_element', '_next'
 
-        def __init__(self, element, next):
+        def __init__(self, element, reference):
+            """
+
+            :param element: value stored in Node
+            :param reference: Pointer to next Node
+            """
             self._element = element
-            self._next = next
+            self._next = reference
 
         def __str__(self):
-            return str(self._element)  # Inside your node class
+            """
 
-        #------------------------------- sparse array methods -------------------------------
+            :return: String value of Node's value
+            """
+            return str(self._element)
+
+            # ------------------------------- SparseArray methods -------------------------------
 
     def __init__(self, size):
+        """
+        Create new SparseArray
+        :param size: size of the array
+        """
         self._data = [None] * size
         self._head = None
         self._size = size
         self._usage = 0
 
+    def __str__(self):
+        """
+
+        :return: formatted string of values SparseArray
+        """
+        array = [str(item) for item in self._data]
+        return "{}".format(array)
+
     def __len__(self):
+        """
+
+        :return: size of the array
+        """
         return self._size
 
     def __iter__(self):
+        """
+
+        :return: Allow iteration of SparseArray
+        """
         return iter(self._data)
 
     def __getitem__(self, item):
+        """
+
+        :param item: item index
+        :return: item at requested index
+        """
         return self._data[item]
 
     def __setitem__(self, key, value):
-        """Add element e to the top of the stack."""
+        """
+        Create Node at selected position with value
+
+        :param key: index position of new value
+        :param value: value of new item
+        :return: None
+        """
         newest = self._Node(value, self._head)  # create and link a new node
         self._data[key] = newest
         self._head = newest
         self._usage += 1
 
     def get_usage(self):
+        """
+
+        :return: Number of positions allocated to Nodes
+        """
         return self._usage
 
     def fill(self, array):
+        """
+        Fill SparseArray with new array values
+        :param array: Array of values to fill
+        :return: None
+        """
         # Check that array will fit within current Arraylist
-        if len(array) > self._size - self._usage:
+        new_array_length = len(array)
+        if new_array_length > self._size - self._usage:
             raise ValueError
         else:
             new_array_counter = 0
             sparse_array_counter = 0
 
-            while new_array_counter != len(array):
+            while new_array_counter != new_array_length:
                 if isinstance(self._data[sparse_array_counter], type(None)):
-                    self._data[sparse_array_counter] = array[new_array_counter]
+                    self.__setitem__(sparse_array_counter, array[new_array_counter])
                     new_array_counter += 1
 
                 sparse_array_counter += 1
-
-
-
-
-
-
---
+++ experiments.py
@@ -0,0 +1,89 @@
+from SparseArray import SparseArray
+from time import time
+from random import randrange
+
+
+def manual_set_get_sparse_array(n, m):
+    """
+    Manually set and get random items from sparse array
+
+    :param n: size of SparseArray
+    :param m: Memory utilised in Sparse Array
+    :return: Average run-time of random get and set actions
+    """
+    time_start = time()
+    s_array = SparseArray(n)
+    times_accessed = 0
+    for index in range(m):
+        rand_index = randrange(n)
+        times_accessed += 1
+        # set random position in SparseArray to random value
+        s_array[rand_index] = rand_index
+        # Access random position in SparseArray
+        rand_item = s_array[rand_index // 2]
+
+    time_end = time()
+    return (time_end - time_start) / times_accessed
+
+def fill_sparse_experiment(n, m):
+    """
+    Measure run-time of fill method with different sizes of n and m
+
+    :param n: size of SparseArray
+    :param m: Memory utilised in Sparse Array
+    :return: run-time of fill method
+    """
+    s_array = SparseArray(n)
+    array = ["blah"] * m
+    time_start = time()
+    s_array.fill(array)
+    time_end = time()
+    return time_end - time_start
+
+def set_element_sparse(n):
+    """
+    Measure a set operation in a random position within sparse array of n size
+
+    :param n: size of Sparse Array
+    :return: time of random set operation in SparseArray
+    """
+    s_array = SparseArray(n)
+    rand_index = randrange(n)
+    time_start = time()
+    s_array[rand_index] = rand_index
+    time_end = time()
+    return time_end - time_start
+
+def get_element_sparse(n):
+    """
+    Measure a get operation in a random position within sparse array of n size
+
+    :param n: size of Sparse Array
+    :return: time of random get operation in SparseArray
+    """
+    s_array = SparseArray(n)
+    rand_index = randrange(n)
+    time_start = time()
+    rand_item = s_array[rand_index]
+    time_end = time()
+    return time_end - time_start
+
+
+print("----------------------------------------------------")
+
+# Unable to create a SparseArray bigger than 10^8
+for i in range(3, 9):
+    print("Sparse Array Average of {0:.3f} with n = 10^{1} and m = 5^{2}".format(manual_set_get_sparse_array(10 ** i, 5 ** i) * 1000000, i, i))
+#
+print("----------------------------------------------------")
+
+
+for i in range(3, 9):
+    print("Sparse fill of {0:.3f} with n = 10^{1} and m = 5^{2}".format(fill_sparse_experiment(10 ** i, 5 ** i) * 1000000, i, i))
+
+
+for i in range(3, 9):
+    print("Sparse set time of {0:.3f} with n = 10^{1}".format(set_element_sparse(10 ** i) * 1000000, i, i))
+
+for i in range(3, 9):
+    print("Sparse get time of {0:.3f} with n = 10^{1}".format(get_element_sparse(10 ** i) * 1000000, i, i))
